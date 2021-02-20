eabdc1f98bd63063950033da0c68528ead3ac954
anqiansong <anqiansong@xiaoheiban.cn>
2021-02-20 11:41:15 +0800

    add build action

---
+++ .github/workflows/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+REPO="https://${ACCESSTOKEN}@github.com:zeromicro/go-zero.git"
+
+# git 配置
+echo "git基础配置"
+git config --local user.name "anqiansong"
+git config --local user.email "anqiansong@tal.com"
+
+# 安装gitbook
+echo "gitbook installing..."
+npm install gitbook-cli -g
+gitbook --version
+gitbook install
+
+# build
+echo "gitbook building..."
+gitbook build
+
+# clone&push
+
+mkdir ./doc
+cd ./doc
+echo $PWD
+echo "document clone..."
+git clone ${REPO}
+cd go-zero
+rm -rf ./*
+cp -rf ../../_book/* .
+git add ./*
+git commit -m 'update document'
+echo "document push..."
+git push -f
